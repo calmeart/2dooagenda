8e47fe19bfd50b87336be03716fa88e37624f7ac
batuhan öztürk <batuhandoruk91@gmail.com>
2021-03-02 01:29:29 +0200

    finalize home page design

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -13,43 +13,41 @@ function getDate(date = new Date()) {
 
 module.exports = (app) => {
   app.get('/', function(req, res) {
-    const day = getDate();
-    Task.find({}).sort({date: 'asc'}).exec(function(err, docs) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("home", {
-          listTitle: "Tasks To Do",
-          listItems: docs,
-          listDate: new Date().toISOString().slice(0,10)
-        });
-      }
-    })
+    const date = new Date();
+    res.render("home");
   });
 
   app.get("/about", function(req, res) {
     res.render("about", {
-      listDate: new Date().toISOString().slice(0,10)
+      listDate: new Date().toISOString().slice(0, 10)
     });
   });
 
-  app.get("/contact", function(req,res){
+  app.get("/contact", function(req, res) {
     res.render("contact", {
       contactMessage: "Please fill the below form",
-      listDate: new Date().toISOString().slice(0,10)
+      listDate: new Date().toISOString().slice(0, 10)
     });
   });
 
-  app.get("/contact/success", function(req,res){
+  app.get("/contact/success", function(req, res) {
     res.render("contact", {
       contactMessage: "Your message has been sent",
-      listDate: new Date().toISOString().slice(0,10)
+      listDate: new Date().toISOString().slice(0, 10)
     });
   });
 
-  app.post("/contact", async function(req,res){
-    const {userName, userMail, message} = req.body;
-    const tempMessage = new Message({userName, userMail, message});
+  app.post("/contact", async function(req, res) {
+    const {
+      userName,
+      userMail,
+      message
+    } = req.body;
+    const tempMessage = new Message({
+      userName,
+      userMail,
+      message
+    });
     await tempMessage.save();
     res.redirect("/contact/success");
   })
