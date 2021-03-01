b2c23425ad607d33c7bd8f3e3b60149f5c0fb46e
batuhan öztürk <batuhandoruk91@gmail.com>
2021-03-02 00:50:29 +0200

    create header clock add dom folder

---
+++ public/dom/home-clock.js
@@ -0,0 +1,21 @@
+
+
+const clockHeader = document.getElementById('heading');
+
+const dateOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+document.body.onload = runningClock;
+
+function runningClock() {
+  const date = new Date().toLocaleDateString('en-GB', dateOptions);
+  const time = new Date().toLocaleTimeString('en-GB', { hour12: false });
+  const string = date + " - " + time;
+  clockHeader.innerHTML = string;
+}
+
+setInterval(runningClock, 1000);
